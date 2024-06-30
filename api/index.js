ed7fb672225c30dec94cc0d58ab2e36a47b81cd9
hocinedev <hocinedev4@gmail.com>
2024-06-30 20:23:56 +0100

    connect db

--- api/index.js
+++ api/index.js
@@ -1,13 +1,23 @@
-const express = require('express');
-const app=express();
+const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
-
 dotenv.config();
 
+async function connectToDatabase() {
+    try {
+      await mongoose.connect(process.env.MONGO_URL);
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("Error connecting to MongoDB", error);
+    }
+  }
+  
+  connectToDatabase();
 
-
-app.listen(8800,()=>{console.log('backend server is running !')})
+app.listen(8800, () => {
+  console.log("backend server is running !");
+});
