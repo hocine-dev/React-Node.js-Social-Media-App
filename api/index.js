1fc11c5df1098a89c9406e210dc2c90b210d4980
hocinedev <hocinedev4@gmail.com>
2024-06-30 19:38:10 +0100

    first

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app=express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+
+dotenv.config();
+
+
+
+app.listen(8800,()=>{console.log('backend server is running !')})
